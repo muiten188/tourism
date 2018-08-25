34d5824fb4cb49857af575c6550c926436b3dcd3
DESKTOP-VJMRKP6\muite <buidinhbach123@gmail.com>
2018-08-25 20:28:44 +0700

    quyền vị trí

--- App.js
+++ App.js
@@ -12,11 +12,36 @@ import {
   View
 } from 'react-native';
 import Index from './src';
+import { PermissionsAndroid } from 'react-native';
 
-export default class App extends Component{
+
+export default class App extends Component {
+
+  componentWillMount() {
+    this.requestCameraPermission();
+  }
   render() {
     return (
       <Index />
     );
+  }
+
+  async requestCameraPermission() {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
+        {
+          'title': 'Thông báo cấp quyền',
+          'message': 'Ứng dụng cần quyền vị trí để thao tác với beacon'
+        }
+      )
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("You can use the camera")
+      } else {
+        console.log("Camera permission denied")
+      }
+    } catch (err) {
+      console.warn(err)
+    }
   }
 }
